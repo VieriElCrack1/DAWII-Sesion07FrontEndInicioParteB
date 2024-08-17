d83e9d258f33b1f962a360d4b87b0ac22b352a01
VieriElCrack1 <santosvieri9@gmail.com>
2024-08-16 19:20:59 -0500

    ordenando proyecto

---
+++ src/app/components/consulta-docente/consulta-docente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-docente',
+  standalone: true,
+  imports: [],
+  templateUrl: './consulta-docente.component.html',
+  styleUrl: './consulta-docente.component.css'
+})
+export class ConsultaDocenteComponent {
+
+}
